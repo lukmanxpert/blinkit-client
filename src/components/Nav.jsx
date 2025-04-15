a369debea8aa952d2c04c2d352b5237e3efd7e67
lukmanxpert <lukmanmiah2004@gmail.com>
2025-04-15 23:57:45 +0600

    installed react type animation and impliment it

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from "../assets/logo.png"
+import { FaSearch } from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation';
 
 const Nav = () => {
   return (
@@ -11,11 +13,38 @@ const Nav = () => {
         </div>
         {/* search */}
         <div>
-
+          <div className='flex justify-start items-center gap-4 p-2 border border-neutral-300 text-neutral-600 rounded-lg w-[300px] md:w-[400px]'>
+            <button className='cursor-pointer'>
+              <FaSearch />
+            </button>
+            <div>
+              <TypeAnimation
+                sequence={[
+                  'Search "Milk"',
+                  1000,
+                  'Search "Bread"',
+                  1000,
+                  'Search "Sugar"',
+                  1000,
+                  'Search "Chocolate"',
+                  1000,
+                  'Search "Egg"',
+                  1000,
+                  'Search "Oil"',
+                  1000,
+                  'Search "Noodles"',
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </div>
+          </div>
         </div>
         {/* login and cart */}
         <div>
-
+          <p>Login And My Cart</p>
         </div>
       </div>
     </header>
