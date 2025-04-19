ba9ab68afa8a67093b1d7db1e7350363d937dea8
lukmanxpert <lukmanmiah2004@gmail.com>
2025-04-19 23:58:59 +0600

    remove unnecessery files

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -34,6 +34,8 @@ const Register = () => {
     console.log("Response", response);
   }
   // validate value
+
+  
   const validateValue = Object.values(formData).every(el => el)
   return (
     <div className='bg-white mt-2 flex flex-col gap-2 items-center'>
