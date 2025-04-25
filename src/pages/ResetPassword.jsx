d3b7b612c0ba9bd8333462276f26d9ce1c7a797d
lukmanxpert <lukmanmiah2004@gmail.com>
2025-04-25 23:59:31 +0600

    added one more validation

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
 const ResetPassword = () => {
+    const [data, setData] = useState({
+        email: "",
+        newPassword: "",
+        confirmPassword: ""
+    })
     const navigate = useNavigate()
     const location = useLocation();
     useEffect(() => {
         if (!(location?.state?.data?.success)) {
             navigate("/")
+        }
+        if (location?.state?.email) {
+            setData((prevData)=>({...prevData, email: location.state.email}))
         }
     }, [])
     console.log(location);
