2146ec54b295d4318bcd642a3a387ec5e87bbc0d
lukmanxpert <lukmanmiah2004@gmail.com>
2025-05-23 23:59:55 +0600

    added product slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import productReducer from "./userSlice"
 
 export const store = configureStore({
   reducer: {
