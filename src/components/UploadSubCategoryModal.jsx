4df8a870947cc1db295a16b3a8e71dc3bb701fdf
lukmanxpert <lukmanmiah2004@gmail.com>
2025-05-23 23:42:42 +0600

    upload image to cloud

--- src/components/UploadSubCategoryModal.jsx
+++ src/components/UploadSubCategoryModal.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { IoClose } from "react-icons/io5";
+import uploadImage from '../utils/uploadImage';
+import toast from 'react-hot-toast';
 
 const UploadSubCategoryModal = ({ close }) => {
+    const [loading, setLoading] = useState(false)
     const [subCategoryData, setSubCategoryData] = useState({
         name: "",
         image: "",
@@ -13,9 +16,23 @@ const UploadSubCategoryModal = ({ close }) => {
         setSubCategoryData((prev) => ({ ...prev, [name]: value }))
     }
     // handleUploadSubCategoryImage
-    const handleUploadSubCategoryImage = (event) => {
-        
+    const handleUploadSubCategoryImage = async (event) => {
+        const file = event.target.files[0]
+        if (!file) {
+            return
+        }
+        setLoading(true)
+        const response = await uploadImage(file)
+        const { data: imageResponse } = response
+        if (!imageResponse?.data?.url) {
+            setLoading(false)
+            return toast.error("Something went wrong, Try Again")
+        }
+        setSubCategoryData(prevData => ({ ...prevData, image: imageResponse.data.url }))
+        setLoading(false)
     }
+    const validateValue = Object.values(subCategoryData).every(el => el)
+    console.log(subCategoryData);
     return (
         <section className='fixed top-0 left-0 right-0 bottom-0 bg-neutral-700/60 z-50 flex justify-center items-center'>
             <div className='bg-white w-full max-w-5xl p-4'>
@@ -30,14 +47,14 @@ const UploadSubCategoryModal = ({ close }) => {
                             <input className='border px-3 py-1 rounded outline-none focus-within:border-primary-100' value={subCategoryData.name} onChange={handleChange} type="text" name='name' id='name' placeholder='Enter the sub category name' />
                         </div>
                         <div className='flex gap-6 mt-4'>
-                            <div className='w-36 min-h-36 bg-blue-100 flex justify-center items-center text-neutral-400'>
+                            <div className='w-36 min-h-36 border-2 border-primary-100 rounded flex justify-center items-center text-neutral-400'>
                                 {
-                                    subCategoryData.image ? <img src={subCategoryData.image} alt="" /> : "No mage"
+                                    subCategoryData.image ? <img src={subCategoryData.image} alt="" /> : <p>No image</p>
                                 }
                             </div>
                             <div>
-                                <button className='border-2 cursor-pointer border-primary-100 px-3 py-1 rounded hover:bg-primary-100 transition' type='button'><label htmlFor="image" className='font-semibold'>Upload Photo</label></button>
-                                <input className='hidden' value={subCategoryData.image} onChange={handleUploadSubCategoryImage} type="file" name='image' id='image' />
+                                <button className='border-2 cursor-pointer border-primary-100 px-3 py-1 rounded hover:bg-primary-100 transition' type='button'><label htmlFor="image" className='font-semibold cursor-pointer'>{loading ? "Uploading" : subCategoryData.image ? "Change Image" : "Upload Image"}</label></button>
+                                <input className='hidden' onChange={handleUploadSubCategoryImage} type="file" name='image' id='image' />
                             </div>
                         </div>
                     </form>
