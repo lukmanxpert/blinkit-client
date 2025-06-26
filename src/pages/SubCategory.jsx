7ce651296ac492fa34411f332668386127d4ad51
lukmanxpert <lukmanmiah2004@gmail.com>
2025-06-26 14:29:23 +0600

    added table for sub category

--- src/pages/SubCategory.jsx
+++ src/pages/SubCategory.jsx
@@ -3,11 +3,14 @@ import UploadSubCategoryModal from '../components/UploadSubCategoryModal'
 import axiosToastError from '../utils/AxiosToastError'
 import Axios from '../utils/Axios'
 import summaryApi from '../common/summaryApi'
+import DisplayTable from '../components/DisplayTable'
+import { createColumnHelper } from "@tanstack/react-table"
 
 const SubCategory = () => {
   const [openAddSubCategory, setOpenAddSubCategory] = useState(false)
   const [subCategoryData, setSubCategoryData] = useState([])
   const [loading, setLoading] = useState(false)
+  const columnHelper = createColumnHelper()
   const fetchSubCategory = async () => {
     try {
       setLoading(true)
@@ -27,13 +30,34 @@ const SubCategory = () => {
   useEffect(() => {
     fetchSubCategory()
   }, [])
+  // column helper
+  const column = [
+    columnHelper.accessor('name', {
+      header: "Name"
+    }),
+    columnHelper.accessor('image', {
+      header: "Image",
+      cell: ({ row }) => {
+        return <div className='flex justify-center items-center'>
+          <img src={row.original.image} alt={row.original.name} className='w-8 h-8' />
+        </div>
+      }
+    }),
+    columnHelper.accessor('category', {
+      header: "Category"
+    }),
+  ]
+  console.log("sub category", subCategoryData);
   return (
     <section>
       <div className='flex justify-between shadow p-2'>
         <h1 className='font-semibold'>Sub Category</h1>
         <button onClick={() => {
           setOpenAddSubCategory(true)
-        }} className='border border-primary-100 px-3 py-1 rounded text-sm hover:bg-primary-100 hover:text-white transition cursor-pointer'>Add Sub Category</button>
+        }} className='border-2 font-semibold border-primary-100 px-3 py-1 rounded text-sm hover:bg-primary-100 hover:text-white transition cursor-pointer'>Add Sub Category</button>
+      </div>
+      <div>
+        <DisplayTable data={subCategoryData} column={column} />
       </div>
       {
         openAddSubCategory && (
