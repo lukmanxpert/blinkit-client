342465ef9cc5fdd7f6989c52fad8b737d3085334
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-06 13:02:54 +0600

    get product data

--- src/common/summaryApi.js
+++ src/common/summaryApi.js
@@ -81,6 +81,10 @@ const summaryApi = {
     url: "/api/product/create",
     method: "post",
   },
+  getProduct: {
+    url: "/api/product/get",
+    method: "post"
+  }
 };
 
 export default summaryApi;
--- src/pages/ProductAdmin.jsx
+++ src/pages/ProductAdmin.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosToastError from '../utils/AxiosToastError'
+import Axios from '../utils/Axios'
+import summaryApi from '../common/summaryApi'
 
 const ProductAdmin = () => {
+  const [productData, setProductData] = useState([])
+  const [page, setPage] = useState(1)
+  const fetchProductData = async () => {
+    try {
+      const response = await Axios({
+        ...summaryApi.getProduct,
+        data: {
+          page: page,
+        }
+      })
+      const { data: responseData } = response
+      if (responseData.success) {
+        setProductData(response.data)
+      }
+      console.log('responseData :>> ', responseData);
+    } catch (error) {
+      axiosToastError(error)
+    }
+  }
+  useEffect(() => {
+    fetchProductData()
+  }, [])
   return (
     <div>ProductAdmin</div>
   )
