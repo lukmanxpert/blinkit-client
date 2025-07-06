bf2056d631226675eb2056e94c4dcd0bd45ed4d4
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-06 14:35:54 +0600

    get and display products added with paginations

--- src/pages/ProductAdmin.jsx
+++ src/pages/ProductAdmin.jsx
@@ -1,33 +1,78 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axiosToastError from '../utils/AxiosToastError'
 import Axios from '../utils/Axios'
 import summaryApi from '../common/summaryApi'
+import Loading from '../components/Loading'
+import ProductCartAdmin from '../components/ProductCartAdmin'
 
 const ProductAdmin = () => {
   const [productData, setProductData] = useState([])
   const [page, setPage] = useState(1)
+  const [totalPageCount, setTotalPageCount] = useState(1)
+  const [loading, setLoading] = useState(false)
   const fetchProductData = async () => {
     try {
+      setLoading(true)
       const response = await Axios({
         ...summaryApi.getProduct,
         data: {
           page: page,
+          limit: 12
         }
       })
       const { data: responseData } = response
       if (responseData.success) {
-        setProductData(response.data)
+        setTotalPageCount(responseData.totalNoPage)
+        setLoading(false)
+        setProductData(response.data.data)
       }
-      console.log('responseData :>> ', responseData);
     } catch (error) {
       axiosToastError(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const handleNext = () => {
+    if (page !== totalPageCount) {
+      setPage(prev => prev + 1)
+    }
+  }
+  const handlePrev = () => {
+    if (page > totalPageCount) {
+      setPage(prev => prev - 1)
     }
   }
   useEffect(() => {
     fetchProductData()
-  }, [])
+  }, [page])
+  console.log('productData :>> ', productData);
   return (
-    <div>ProductAdmin</div>
+    <section>
+      <div className='flex justify-between shadow p-2'>
+        <h1 className='font-semibold'>Products</h1>
+      </div>
+      {
+        loading && <Loading />
+      }
+      <div className='p-4 bg-blue-50'>
+        <div className='min-h-[55vh]'>
+          <div className='grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6'>
+            {
+              productData && productData.map((product, index) => {
+                return (
+                  <ProductCartAdmin data={product} key={index} />
+                )
+              })
+            }
+          </div>
+        </div>
+        <div className='flex justify-end gap-4 my-4'>
+          <button onClick={handlePrev} className='border font-semibold border-primary-200 px-4 py-1 hover:bg-primary-200 transition cursor-pointer'>Previous</button>
+          <button>{page}/{totalPageCount}</button>
+          <button onClick={handleNext} className='border font-semibold border-primary-200 px-4 py-1 hover:bg-primary-200 transition cursor-pointer'>Next</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
